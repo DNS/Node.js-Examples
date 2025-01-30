d94840f164e8815030e7319c37264d4787a12067
DNS <37093+DNS@users.noreply.github.com>
2025-01-30 13:36:30 +0700

    variables

---
+++ variables.js
@@ -0,0 +1,12 @@
+﻿var a = 'hello'
+var b = "world"
+c = 'welcome'
+d = 1234
+e = 0.5678
+
+
+console.log(a+b+c+d+e) 
+
+// ; statement separator
+console.log('statement'); console.log('separator')
+

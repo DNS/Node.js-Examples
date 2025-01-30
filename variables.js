b30dfca48031e2f2eb0dea6bdbe2a03c019358b8
DNS <37093+DNS@users.noreply.github.com>
2025-01-30 14:24:59 +0700

    file, exception

--- variables.js
+++ variables.js
@@ -1,15 +1,14 @@
-﻿
-/* 
-    variable declaration scope
+﻿/* 
     var -> function scope
     let -> block scope (loop)
+    const -> constant
 */
 
 var a = 'hello'
 let b = "world"
 c = 'welcome'
 d = 1234
-e = 0.5678
+const e = 0.5678
 var arr1 = [1,2,3,4]
 var arr2 = ['welcome','to','indonesia']
 
@@ -69,5 +68,31 @@ function TestFunction (x) {
 TestFunction(1234)
 
 // lambda
-lambda = function (x) {console.log('from anonymous function: ' + x)}
-lambda(1234)
+lambda1 = function (x) {console.log('from anonymous function: ' + x)}
+lambda1('ok 1')
+
+lambda2 = (x) => {console.log('from anonymous function: ' + x)}
+lambda2('ok 1')
+
+
+// file operation
+const fs = require('fs')
+fs.writeFileSync('test.txt', 'hello world')
+var buf = fs.readFile('test.txt', function(){})
+fs.close()
+
+
+fs.readFile('test.txt', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log(data);
+  });
+
+// exception
+try {
+    console.log('try something')
+} catch (err) {
+    console.err('handle error')
+}

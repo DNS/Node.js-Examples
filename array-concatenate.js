b4e7ce23923f782bf578af06bbaa0ccdcf92aac6
DNS <37093+DNS@users.noreply.github.com>
2025-03-18 23:56:23 +0700

    array-concatenate.js

---
+++ array-concatenate.js
@@ -0,0 +1,12 @@
+
+
+// ... spread operator to combine array
+a = [5,4]
+b = [2,3]
+c = [...a,...b]
+console.log( c )
+
+
+// In function definitions, it is used to define a function that takes a variable number of arguments: func(...args).
+
+
